6bf046680ef733cdc7d5659a3b1b4b3022e8c7f0
Rose Pearson <rose.pearson@niwa.co.nz>
2021-06-30 14:40:14 +1200

    created a basic test and made sure it runs - but don't yet properly check to see if the test passes. The test must be run form the base repo directory to work

--- tests/test1/test1.py
+++ tests/test1/test1.py
@@ -7,16 +7,21 @@ Created on Wed Jun 30 11:11:25 2021
 
 import unittest
 import json
+import pathlib
 
-import GeoFabrics
+from src import GeoFabrics
+#from src.GeoFabrics import processor
+import geopandas
 
 class Test1(unittest.TestCase):
 
     def test_add(self):
         
-        with open("instructions.json", 'r') as file_pointer:
-            instructions = json.load(file_pointer)
+        file_path = pathlib.Path().cwd() / pathlib.Path("tests/test1/instruction.json")
         
+        with open(file_path, 'r') as file_pointer:
+            instructions = json.load(file_pointer)
+            
         runner = GeoFabrics.processor.GeoFabricsGenerator(instructions)
         runner.run()
         #self.assertEqual((Number(5) + Number(6)).value, 11)
