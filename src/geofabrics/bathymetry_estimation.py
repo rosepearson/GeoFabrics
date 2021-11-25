1f548e42f1f07dda764a57edab26db1ab20d7c52
Rose Pearson <Rose.Pearson@niwa.co.nz>
2021-11-26 09:14:46 +1300

    minor correction to inwards width detection algorithm

--- src/geofabrics/bathymetry_estimation.py
+++ src/geofabrics/bathymetry_estimation.py
@@ -407,10 +407,10 @@ class ChannelBathymetry:
 
                 # work forward checking height
                 if transect_samples['elevations'][j][i] - transects.loc[j]['min_z'] > threshold:
-                    stop_i = i
+                    stop_i = len(transect_samples['elevations'][j]) - i - 1
                 # work backward checking height
                 if transect_samples['elevations'][j][-i] - transects.loc[j]['min_z'] > threshold:
-                    start_i = len(transect_samples['elevations'][j]) - i - 1
+                    start_i = i
 
             widths['first_widths'].append((centre_index-start_i)*resolution)
             widths['last_widths'].append((stop_i-centre_index)*resolution)
