620bff15d34cf9323369d4d9302b58a4650938c0
Rose Pearson <Rose.Pearson@niwa.co.nz>
2022-06-18 02:33:25 +1200

    update package name

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setuptools.setup(
-    name="GeoFabrics",
+    name="geofabrics",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     author="Rose pearson",
