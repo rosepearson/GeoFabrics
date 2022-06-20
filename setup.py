bd4f8c38b11acbf0b33b61f593de2ba03546dd50
Rose Pearson <rose.pearson@niwa.co.nz>
2022-06-21 05:52:45 +1200

    initial clean of setup and linux env file - make alphabetical order (#80)
    * initial clean of setup and linux env file - make alphabetical order
    
    * update package name

--- setup.py
+++ setup.py
@@ -9,22 +9,23 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setuptools.setup(
-    name="GeoFabrics",
+    name="geofabrics",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     author="Rose pearson",
     author_email="rose.pearson@niwa.co.nz",
-    description="A package for creating hydrologically conditioned geo-fabrics "
-    "(i.e. DEMs and roughness maps)",
+    description="A package for creating geofabrics for flood modelling "
+    "(i.e. Hydrologically conditioned DEMs and roughness maps)",
     keywords="GeoFabrics, DEM, Roughness, Hydrologically conditioned",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rosepearson/GeoFabrics",
     project_urls={
-        "Documentation": "https://github.com/rosepearson/GeoFabrics",
+        "Documentation": "https://rosepearson.github.io/GeoFabrics/",
+        "Wiki": "https://github.com/rosepearson/GeoFabrics/wiki",
         "Bug Reports": "https://github.com/rosepearson/GeoFabrics/issues",
         "Source Code": "https://github.com/rosepearson/GeoFabrics",
     },
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
     classifiers=[
         # see https://pypi.org/classifiers/
         "Development Status :: 2 - Pre-Alpha",
@@ -36,15 +37,15 @@ setuptools.setup(
     ],
     python_requires=">=3.6",
     install_requires=[
-        "numpy>=1.21",
-        "rioxarray>=0.4",
-        "pdal>=2.2",
+        "dask",
+        "distributed",
+        "gdal",
         "geopandas>=0.10",
-        "shapely>=1.7",
-        "scipy>=1.6",
-        "dask>=2021.9",
-        "distributed>=2021.9",
-        "python-dotenv>=0.19",
+        "pdal",
+        "pytest",
+        "python-dotenv",
+        "python-pdal",
+        "rioxarray",
     ],
     extras_require={
         "dev": ["check-manifest"],
