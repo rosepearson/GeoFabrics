554754219a2d3aa81e7fcf00c1149dc0df8ed6a1
Rose Pearson <Rose.Pearson@niwa.co.nz>
2022-09-20 12:26:45 +1200

    Update formatting and replace outdated shapely casecaded union with a unary union call

--- src/geofabrics/geometry.py
+++ src/geofabrics/geometry.py
@@ -194,7 +194,7 @@ class CatchmentGeometry:
         # any sub-pixel polygons.
         offshore_with_lidar = dense_extents.clip(self.offshore, keep_geom_type=True)
         offshore_with_lidar = offshore_with_lidar[
-            offshore_with_lidar.area > self.resolution**2
+            offshore_with_lidar.area > self.resolution ** 2
         ]
         offshore_without_lidar = geopandas.overlay(
             self.offshore, offshore_with_lidar, how="difference"
@@ -218,17 +218,15 @@ class CatchmentGeometry:
         offshore_foreshore_dense_data_extents = geopandas.GeoDataFrame(
             {
                 "geometry": [
-                    shapely.ops.cascaded_union(
+                    shapely.ops.unary_union(
                         [self.foreshore.loc[0].geometry, dense_extents.loc[0].geometry]
                     )
                 ]
             },
             crs=self.crs["horizontal"],
         )
-        offshore_foreshore_dense_data_extents = (
-            offshore_foreshore_dense_data_extents.clip(
-                self.foreshore_and_offshore, keep_geom_type=True
-            )
+        offshore_foreshore_dense_data_extents = offshore_foreshore_dense_data_extents.clip(
+            self.foreshore_and_offshore, keep_geom_type=True
         )
 
         # deflate this - this will be taken away from the
@@ -310,7 +308,7 @@ class BathymetryContours:
                 self.catchment_geometry.offshore, keep_geom_type=True
             )
             exclusion_extent = exclusion_extent[
-                exclusion_extent.area > self.catchment_geometry.resolution**2
+                exclusion_extent.area > self.catchment_geometry.resolution ** 2
             ]
             self._extent = self.catchment_geometry.offshore.overlay(
                 exclusion_extent, how="difference"
@@ -714,7 +712,7 @@ class RiverMouthFan:
         # Calculate the normal and tangent to the channel segment at the mouth
         segment_dx = x[0] - x[1]
         segment_dy = y[0] - y[1]
-        segment_length = numpy.sqrt(segment_dx**2 + segment_dy**2)
+        segment_length = numpy.sqrt(segment_dx ** 2 + segment_dy ** 2)
         mouth_tangent = shapely.geometry.Point(
             [segment_dx / segment_length, segment_dy / segment_length]
         )
