6736f8cc5be8dd2a8bb1191e8a4281afeb5eff77
Rose Pearson <Rose.Pearson@niwa.co.nz>
2022-09-29 11:29:08 +1300

    Improved logging and pulled running processing out to a separate function

--- src/main.py
+++ src/main.py
@@ -36,7 +36,7 @@ def parse_args():
     return parser.parse_args()
 
 
-def setup_logging_for_run(instructions: dict):
+def setup_logging_for_run(instructions: dict, label: str):
     """Setup logging for the current processor run"""
 
     assert "local_cache" in instructions["data_paths"], (
@@ -44,15 +44,19 @@ def setup_logging_for_run(instructions: dict):
         "this is where the log file will be written."
     )
 
-    log_path = pathlib.Path(instructions["data_paths"]["local_cache"])
+    log_path = pathlib.Path(pathlib.Path(instructions["data_paths"]["local_cache"]))
+    if "subfolder" in instructions["data_paths"].keys():
+        log_path = log_path / instructions["data_paths"]["subfolder"]
+    else:
+        log_path = log_path / "results"
     log_path.mkdir(parents=True, exist_ok=True)
     logging.basicConfig(
-        filename=log_path / "geofabrics.log",
+        filename=log_path / f"geofabrics_{label}.log",
         encoding="utf-8",
         level=logging.INFO,
         force=True,
     )
-    print(f"Log file is located at: {log_path / 'geofabrics.log'}")
+    print(f"Log file is located at: geofabrics_{label}.log")
     logging.info(instructions)
 
 
@@ -101,6 +105,25 @@ def check_for_benchmarks(instructions: dict, runner: processor.BaseProcessor):
         )
 
 
+def run_processor_class(processor_class, processor_label: str, instructions: dict):
+    """Run a processor class recording outputs in a unique log file and timing the
+    execution. """
+
+    start_time = time.time()
+    print(f"Run {processor_class.__name__} at {start_time}")
+    run_instructions = instructions[processor_label]
+    setup_logging_for_run(instructions=run_instructions, label=processor_label)
+    runner = processor_class(run_instructions)
+    runner.run()
+    message = (
+        f"Execution time is {time.time() - start_time} for "
+        f"{processor_class.__name__}"
+    )
+    print(message)
+    logging.info(message)
+    return runner
+
+
 def launch_processor(args):
     """Run the DEM generation pipeline(s) given the specified instructions.
     If a benchmark is specified compare the result to the benchmark"""
@@ -112,34 +135,19 @@ def launch_processor(args):
     initial_start_time = time.time()
     if "rivers" in instructions:
         # Estimate river channel bathymetry
-        start_time = time.time()
-        print(f"Run processor.RiverBathymetryGenerator at {start_time}")
-        run_instructions = instructions["rivers"]
-        setup_logging_for_run(run_instructions)
-        runner = processor.RiverBathymetryGenerator(run_instructions)
-        runner.run()
-        message = (
-            f"Execution time is {time.time() - start_time} for "
-            "processor.RiverBathymetryGenerator"
+        run_processor_class(
+            processor_class=processor.RiverBathymetryGenerator,
+            processor_label="rivers",
+            instructions=instructions,
         )
-        print(message)
-        logging.info(message)
     if "waterways" in instructions:
         # Estimate waterway elevations
-        start_time = time.time()
-        print("Run processor.WaterwayBedElevationEstimator")
-        run_instructions = instructions["waterways"]
-        setup_logging_for_run(run_instructions)
-        runner = processor.WaterwayBedElevationEstimator(run_instructions)
-        runner.run()
-        message = (
-            f"Execution time is {time.time() - start_time} for "
-            "processor.WaterwayBedElevationEstimator"
+        run_processor_class(
+            processor_class=processor.WaterwayBedElevationEstimator,
+            processor_label="waterways",
+            instructions=instructions,
         )
-        print(message)
-        logging.info(message)
     if "dem" in instructions:
-        start_time = time.time()
         run_instructions = instructions["dem"]
         dem_paths = run_instructions["data_paths"]
         if "raw_dem" not in dem_paths or not (
@@ -151,44 +159,25 @@ def launch_processor(args):
             ).is_file()
         ):
             # Create a raw DEM from LiDAR / reference DEM
-            start_time = time.time()
-            print("Run processor.RawLidarDemGenerator")
-            setup_logging_for_run(run_instructions)
-            runner = processor.RawLidarDemGenerator(run_instructions)
-            runner.run()
-            message = (
-                f"Execution time is {time.time() - start_time} for "
-                "processor.RawLidarDemGenerator"
+            run_processor_class(
+                processor_class=processor.RawLidarDemGenerator,
+                processor_label="dem",
+                instructions=instructions,
             )
-            print(message)
-            logging.info(message)
         # Add bathymetry information to a raw DEM
-        start_time = time.time()
-        print("Run processor.HydrologicDemGenerator")
-        setup_logging_for_run(run_instructions)
-        runner = processor.HydrologicDemGenerator(run_instructions)
-        runner.run()
-        message = (
-            f"Execution time is {time.time() - start_time} for "
-            "processor.HydrologicDemGenerator"
+        runner = run_processor_class(
+            processor_class=processor.HydrologicDemGenerator,
+            processor_label="dem",
+            instructions=instructions,
         )
-        print(message)
-        logging.info(message)
         check_for_benchmarks(run_instructions, runner)
     if "roughness" in instructions:
         # Create a roughness map and add to the hydrological DEM
-        start_time = time.time()
-        print("Run processor.RoughnessLengthGenerator")
-        run_instructions = instructions["roughness"]
-        setup_logging_for_run(run_instructions)
-        runner = processor.RoughnessLengthGenerator(run_instructions)
-        runner.run()
-        message = (
-            f"Execution time is {time.time() - start_time} for "
-            "processor.RoughnessLengthGenerator"
+        run_processor_class(
+            processor_class=processor.RoughnessLengthGenerator,
+            processor_label="roughness",
+            instructions=instructions,
         )
-        print(message)
-        logging.info(message)
     print(f"Total execution time is {time.time() - initial_start_time}")
 
 
