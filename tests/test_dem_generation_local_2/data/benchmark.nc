6ccb13163da30b9ed04f82efe4ef757ecd6e25c0
rosepearson <rose.pearson@niwa.co.nz>
2024-08-08 13:52:22 +1200

    updated tests

---
+++ tests/test_dem_generation_local_2/data/benchmark.nc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fe5571b9dc36c3220e8b35f9baf6f3509210d008bfc8fc2729103d89688879f4
+size 72576
--- tests/test_dem_generation_local_2/test_case.py
+++ tests/test_dem_generation_local_2/test_case.py
@@ -130,54 +130,6 @@ class Test(base_test.Test):
         runner = processor.HydrologicDemGenerator(cls.instructions)
         runner.run()
 
-    @classmethod
-    def tearDownClass(cls):
-        """Remove created files in the cache directory as part of the testing process at
-        the end of the test."""
-        gc.collect()
-        cls.clean_data_folder()
-
-    @classmethod
-    def clean_data_folder(cls):
-        """Remove all generated or downloaded files from the data directory,
-        but with only warnings if files can't be removed."""
-
-        assert cls.cache_dir.exists(), (
-            "The data directory that should include the comparison benchmark dem file "
-            "doesn't exist"
-        )
-
-        # Cycle through all folders within the cache dir deleting their contents
-        for path in cls.cache_dir.iterdir():
-            if path.is_dir():
-                for file in path.glob("*"):  # only files
-                    if file.is_file():
-                        try:
-                            file.unlink()
-                        except (Exception, PermissionError) as caught_exception:
-                            logging.warning(
-                                f"Caught error {caught_exception} during "
-                                f"rmtree of {file}. Supressing error. You "
-                                "will have to manually delete."
-                            )
-                    elif file.is_dir():
-                        try:
-                            shutil.rmtree(file)
-                        except (Exception, PermissionError) as caught_exception:
-                            logging.warning(
-                                f"Caught error {caught_exception} during "
-                                f"rmtree of {file}. Supressing error. You "
-                                "will have to manually delete."
-                            )
-                try:
-                    shutil.rmtree(path)
-                except (Exception, PermissionError) as caught_exception:
-                    logging.warning(
-                        f"Caught error {caught_exception} during rmtree of "
-                        f"{path}. Supressing error. You will have to manually "
-                        "delete."
-                    )
-
     def test_result_dem(self):
         """A basic comparison between the generated and benchmark DEM"""
 
